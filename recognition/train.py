48999d09ac0685c6c52163ef56189d16da8a84af
Kenji Kawanobe <kenji_kawanobe@jsl.co.jp>
2019-12-24 18:42:52 +0900

    レポジトリ整理

---
+++ recognition/train.py
@@ -0,0 +1,81 @@
+from __future__ import print_function
+import keras
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization, ZeroPadding2D
+from keras import backend as K
+import argparse
+from load_images import load_images_from_labelFolder
+ 
+batch_size = 20
+num_classes = 7
+epoch = 30
+ 
+img_rows, img_cols = 128, 128
+ 
+ 
+parser = argparse.ArgumentParser()
+# 絶対バスで指定
+# parser.add_argument('--path', '-p', default='/Users/kawanohenkenji/private/study/openCV_practice/images')
+# 相対パスで指定
+parser.add_argument('--path', '-p', default='../images/dataset')
+args = parser.parse_args()
+
+(x_train, y_train), (x_test, y_test) = load_images_from_labelFolder(args.path,img_cols, img_rows, train_test_ratio=(6,1))
+ 
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 3)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 3)
+    input_shape = (img_rows, img_cols, 3)
+ 
+x_train = x_train.astype('float32')
+x_test  = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+ 
+ 
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+ 
+model = Sequential()
+ 
+model.add(Conv2D(32, kernel_size=(3,3),
+            activation='relu',
+            input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+model.add(Dropout(0.2))
+ 
+model.add(ZeroPadding2D(padding=(1, 1)))
+model.add(Conv2D(96, kernel_size=(3,3),activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+model.add(Dropout(0.2))
+ 
+model.add(ZeroPadding2D(padding=(1, 1)))
+model.add(Conv2D(96, kernel_size=(3,3)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+model.add(Flatten())
+ 
+model.add(Dense(units=1024, activation='relu'))
+model.add(Dropout(rate=0.5))
+model.add(Dense(units=num_classes, activation='softmax'))
+ 
+model.compile(loss=keras.losses.categorical_crossentropy,
+                optimizer=keras.optimizers.Adam(),
+                metrics=['accuracy'])
+ 
+model.fit(x_train, y_train,
+            batch_size=batch_size,
+            epochs=epoch,
+            verbose=1,
+            validation_data=(x_test, y_test))
+ 
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+ 
+model.save('mymodel.h5') # 学習済みモデルを保存
